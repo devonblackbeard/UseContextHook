3dcee581021ae3a6cc26a5e8d753c8761203e78f
Devon <devon.blackbeard@gmail.com>
2024-02-10 23:52:10 -0700

    Create demo for useContext

--- src/FunctionalContextComponent.js
+++ src/FunctionalContextComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./input.css";
 import { useTheme, useThemeUpdate } from "./ThemeContext";
 
 const FunctionalContextComponent = () => {
@@ -8,14 +9,19 @@ const FunctionalContextComponent = () => {
   const themeStyles = {
     backgroundColor: darkTheme ? "#333" : "#CCC",
     color: darkTheme ? "#CCC" : "#333",
-    padding: "2rem",
-    margin: "2rem",
+    padding: "1rem",
+    margin: "1rem",
   };
 
   return (
     <>
-      <button onClick={toggleTheme}>Toggle Theme</button>
-      <div style={themeStyles}>Function theme</div>;
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded"
+        onClick={toggleTheme}
+      >
+        Toggle Theme
+      </button>
+      <div style={themeStyles}>Function theme</div>
     </>
   );
 };
